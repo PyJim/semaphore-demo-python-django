e9613887f46643efadd8f6cdcfe7301ff2359d07
Jimmy Essel <jimmy.essel@amalitech.com>
2024-11-14 12:20:27 +0000

    [fix] revert to django nose

--- tasks/tests/test_browser.py
+++ tasks/tests/test_browser.py
@@ -2,8 +2,9 @@
     Unit Test file for views
 """
 from django.test import TestCase
+
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
 from pydjango_ci_integration.settings import SITE_URL
 
 
@@ -11,19 +12,9 @@ class TaskListViewTest(TestCase):
     """
     Test View class
     """
-    databases = {'default'}
-
-    def setUp(self):
-        # Set up Chrome browser with webdriver-manager
-        self.browser = webdriver.Chrome(ChromeDriverManager().install())
-
-    def tearDown(self):
-        # Ensure browser closes after each test
-        self.browser.quit()
-
+    # # Browser Integration testing with Selenium
     def test_chrome_site_homepage(self):
-        """
-        Test if the Chrome browser can load the homepage
-        """
-        self.browser.get(SITE_URL)
-        self.assertIn('Semaphore', self.browser.title)
+        browser = webdriver.Chrome()
+        browser.get(SITE_URL)
+        self.assertIn('Semaphore', browser.title)
+        browser.close()
