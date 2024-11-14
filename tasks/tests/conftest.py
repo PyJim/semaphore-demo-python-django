7df6be7451910fcb1267e6b7d6ea9d572e80da98
Jimmy Essel <jimmy.essel@amalitech.com>
2024-11-14 12:16:49 +0000

    [fix] revert to django nose

--- tasks/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-import django
-from django.conf import settings
-
-def pytest_configure():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pydjango_ci_integration.settings')
-    django.setup()
